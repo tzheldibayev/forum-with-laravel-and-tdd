3cabbbebf1fc4a2feb15066da543dd3177ae5bd0
talgat065 <talgat06585@gmail.com>
2018-06-15 11:33:35 +0000

    add unit test to Reply Model. add view and some relationships between models

--- app/Thread.php
+++ app/Thread.php
@@ -6,8 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
+
+    //public function owner ()
+    //{
+    //    return $this->belongsTo(User::class, 'user_id');
+    //}
     public function path()
     {
-        return 'threads/' . $this->id;
+        return '/threads/' . $this->id;
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
     }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,7 @@ $factory->define(App\User::class, function (Faker $faker) {
     ];
 });
 
-$factory->define(App\Thread::class, function (Faker $faker) {
+$factory->define(App\Thread::class, function ($faker) {
     return [
         'user_id' => function () {
             return factory('App\User')->create()->id;
@@ -32,15 +32,14 @@ $factory->define(App\Thread::class, function (Faker $faker) {
     ];
 });
 
-$factory->define(App\Reply::class, function (Faker $faker) {
+$factory->define(App\Reply::class, function ($faker) {
     return [
+        'thread_id' => function () {
+            return factory('App\Thread')->create()->id;
+        },
         'user_id' => function () {
             return factory('App\User')->create()->id;
         },
-        'thread_id' => function () {
-            return factory('App\Reply')->create()->id;
-        },
         'body' => $faker->paragraph,
     ];
-
 });
