bb74d0788e098e232c4367d1b928770f83a1d7ea
talgat065 <talgat06585@gmail.com>
2018-06-18 08:17:05 +0000

    add reply form in thread page

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -23,5 +23,27 @@
                 @endforeach
             </div>
         </div>
+
+        @if (auth()->check())
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <form method="post" action="{{ $thread->path() . '/replies' }}">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                                <textarea class="form-control" name="body" id="" rows="5" placeholder="Have something to say?"></textarea>
+
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <button type="submit" class="btn btn-default">Post </button>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        @else
+            <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this disscussion</p>
+        @endif
     </div>
 @endsection
