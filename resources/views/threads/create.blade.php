f58a8ecf9032b97c70d90447bf4a18d818ee7a8c
talgat065 <talgat06585@gmail.com>
2018-06-21 11:03:22 +0000

    Add channel field to the create form

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -11,15 +11,35 @@
                         <form action="/threads" method="post" role="form">
                             {{ csrf_field() }}
                             <div class="form-group">
+                                <label for="channel_id">Choose a Channel</label>
+                                <select class="form-control" name="channel_id" id="channel_id" required>
+                                    <option value="">Choose one...</option>
+                                    @foreach(App\Channel::all() as $channel)
+                                        <option
+                                            value="{{ $channel->id }}" {{ old('channel_id')==$channel->id ? 'selected': '' }}>{{ $channel->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
                                 <label for="title">Title</label>
-                                <input type="text" class="form-control" name="title" id="" placeholder="">
+                                <input type="text" class="form-control" name="title" id="title"
+                                       value="{{ old('title') }}" required>
                             </div>
                             <div class="form-group">
                                 <label for="body">Body</label>
-                                <textarea class="form-control" name="body" id="body" rows="8"></textarea>
+                                <textarea class="form-control" name="body" id="body"
+                                          rows="8" required>{{ old('body') }}</textarea>
                             </div>
-
-                            <button type="submit" class="btn btn-primary">Publish</button>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Publish</button>
+                            </div>
+                            @if(count($errors))
+                                <ul class="alert alert-danger">
+                                    @foreach($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            @endif
                         </form>
                     </div>
                 </div>
