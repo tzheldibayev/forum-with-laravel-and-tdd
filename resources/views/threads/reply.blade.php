df81972f4b8713de8627425049237d5679b77d26
talgat065 <talgat06585@gmail.com>
2018-06-26 15:58:44 +0000

    add favorite button to view

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,8 +1,18 @@
 <div class="card">
     <div class="card-header">
-        <a href="#"> {{ $reply->owner->name }} </a>
-        said
-        {{ $reply->created_at->diffForHumans() }}</div>
+        <div class="level">
+            <h5 class="flex"><a href="#"> {{ $reply->owner->name }} </a>
+                said
+                {{ $reply->created_at->diffForHumans() }}</h5>
+            <form method="post" action="/replies/{{ $reply->id }}/favorites">
+                {{ csrf_field() }}
+                <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                    {{ $reply->favorites()->count() }} {{ str_plural('Favorite', $reply->favorites()->count()) }}
+                </button>
+
+            </form>
+        </div>
+    </div>
     <div class="card-body">
         {{ $reply->body }}
     </div>
