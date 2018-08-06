f2296111a6d01d5a19411c47ddb2923341fabec2
talgat065 <talgat06585@gmail.com>
2018-08-06 09:29:58 +0000

    channel fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,9 +16,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \View::composer('*', function($view) {
-            $channels = \Illuminate\Support\Facades\Cache::rememberForever('channels', function() {
-                return Channel::all();
-            });
+            $channels = Channel::all();
+            //$channels = \Illuminate\Support\Facades\Cache::rememberForever('channels', function() {
+            //    return Channel::all();
+            //}); 
            $view->with('channels', $channels);
         });
     }
